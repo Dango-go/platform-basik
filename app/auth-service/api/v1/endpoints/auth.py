3730fef3949e05d4316e07dacf93ead80cc69495
Bohdan <bogdandango917@gmail.com>
2026-09-03 02:09:08 +0300

    env script

--- app/auth-service/api/v1/endpoints/auth.py
+++ app/auth-service/api/v1/endpoints/auth.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession # execute()
 from sqlalchemy.future import select
 from auth.core.db import get_db
-from app.auth.core.security import verify_password, get_password_hash, create_access_token
-from app.auth.models.user import User_table
-from app.auth.schemas.user import UserCreate, UserResponse, Token
+from auth.core.security import verify_password, get_password_hash, create_access_token
+from auth.models.user import User_table
+from auth.schemas.user import UserCreate, UserResponse, Token
 
 router = APIRouter()
 
