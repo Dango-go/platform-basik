95671997df9add957448e319d483c8f1a2736136
Bohdan <bogdandango917@gmail.com>
2026-09-05 12:51:22 +0300

    init table in vault

--- app/vault-service/main.py
+++ app/vault-service/main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI, APIRouter
 from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 from app.api.v1.routers import router_v1
 from fastapi.responses import Response
+from app.core.db import engine, Base
+import app.domain.db_models
 
 app = FastAPI(title="vault-service")
 
 router = APIRouter()
 
 app.include_router(router_v1)
+
+
+@app.on_event("startup")
+def init_tables():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/health")
 def root():
@@ -17,3 +24,4 @@ def root():
 app.get("/metrics", include_in_schema=False)
 def metrics_endpoint():
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
