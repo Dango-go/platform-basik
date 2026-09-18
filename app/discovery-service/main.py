621138351baea83abc076339edefbccf583f216f
Bohdan <bogdandango917@gmail.com>
2026-09-18 13:59:41 +0300

    added migration

--- app/discovery-service/main.py
+++ app/discovery-service/main.py
@@ -5,10 +5,14 @@ import models.db_models
 
 app = FastAPI(title="Cluster Discovery Service", version="1.0.0")
 
+from sqlalchemy import text
+
 @app.on_event("startup")
 async def init_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        await conn.execute(text("ALTER TABLE discovery_db ADD COLUMN IF NOT EXISTS ca_cert VARCHAR;"))
+        await conn.execute(text("ALTER TABLE discovery_db ADD COLUMN IF NOT EXISTS token VARCHAR;"))
 
 app.include_router(discovery_router)
 
